6da54acd332691d83ff39f9a62c594b5139a2c2b
Rodrigo Brito <rodrigo-brito@users.noreply.github.com>
2022-12-18 11:04:33 -0300

    deploy

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Mirror] Atention
+
+This repository is a mirror of GoCity production.
+
+Original source code found here: https://github.com/rodrigo-brito/gocity
---
+++ precache-manifest.5f36816b084f7e1277fe3bff137ef2c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6668ef058b07619b7c71a8d5dba5bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e7c6d1d1364b26f78b1",
+    "url": "/static/css/main.1517fb09.chunk.css"
+  },
+  {
+    "revision": "ae6b48ee5d46f9280f82",
+    "url": "/static/js/2.f52b23d7.chunk.js"
+  },
+  {
+    "revision": "51805d17151b1ee978e360ccfe11a4a6",
+    "url": "/static/js/2.f52b23d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7c6d1d1364b26f78b1",
+    "url": "/static/js/main.d8300f90.chunk.js"
+  },
+  {
+    "revision": "d51cb4a4a6b711196312",
+    "url": "/static/js/runtime-main.2357c2aa.js"
+  },
+  {
+    "revision": "a0aa7ce4d84a2e96ffddc26fa795ffea",
+    "url": "/static/media/logo.a0aa7ce4.png"
+  }
+]);
